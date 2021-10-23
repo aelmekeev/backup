a876427fbcf3b6f95d4d7f8d57274320c5bd9bc1
Aleksandr Elmekeev <donamok@gmail.com>
2021-10-23 18:08:48 +0100

    Add restore files

--- asdf/backup.sh
+++ asdf/backup.sh
@@ -1,4 +1,13 @@
 cp ~/.tool-versions .
 cp ~/.asdfrc .
 
-# TODO: add generation of the restore.sh
+if [ -f .tool-versions ]; then
+  rm -f extra_restore.sh
+  echo "" >>.tool-versions
+
+  while read -r tool; do
+    # TODO: add plugin installation
+    [ ! -z "$tool" ] && echo "asdf install $tool" >>extra_restore.sh
+  done <".tool-versions"
+  chmod +x extra_restore.sh
+fi
