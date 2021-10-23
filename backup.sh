57a320bda259c99b4360ca7bd2d0254c0fa8d959
Aleksandr Elmekeev <donamok@gmail.com>
2021-10-23 18:18:40 +0100

    Add ability to configure scope of backup and restore

--- backup.sh
+++ backup.sh
@@ -1,8 +1,19 @@
-for d in */; do
+function backup() {
+  app_dir=$1
   (
-    cd $d &&
-    echo "Backing up $d"
-    ls -A | grep -vE '(backup|restore).sh' | xargs rm &&   # remove all files except backup.sh and restore.sh
-    [[ -f ./backup.sh ]] && $(./backup.sh || true) || echo "WARNING: no backup.sh in $d"
+    cd $app_dir &&
+      echo "Backing up $app_dir" &&
+      ls -A | grep -vE '(backup|restore).sh' | xargs rm && # remove all files except backup.sh and restore.sh
+      [[ -f ./backup.sh ]] && $(./backup.sh || true) || echo "WARNING: no backup.sh in $app_dir"
   )
-done
+}
+
+if [ -f apps.conf ]; then
+  while read -r app_dir; do
+    backup $app_dir
+  done <"apps.conf"
+else
+  for app_dir in */; do
+    backup $app_dir
+  done
+fi
--- restore.sh
+++ restore.sh
@@ -1,7 +1,18 @@
-for d in */; do
+function restore() {
+  app_dir=$1
   (
-    cd $d &&
-    echo "Restoring $d"
-    [[ -f ./restore.sh ]] && $(./restore.sh || true) || echo "WARNING: no restore.sh in $d"
+    cd $app_dir &&
+      echo "Restoring $app_dir" &&
+      [[ -f ./restore.sh ]] && $(./restore.sh || true) || echo "WARNING: no restore.sh in $app_dir"
   )
-done
+}
+
+if [ -f apps.conf ]; then
+  while read -r app_dir; do
+    restore $app_dir
+  done <"apps.conf"
+else
+  for app_dir in */; do
+    restore $app_dir
+  done
+fi
