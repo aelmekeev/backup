11c33b06342509c45193df052cab28df17f1b96e
Aleksandr Elmekeev <donamok@gmail.com>
2021-10-23 17:35:26 +0100

    Fix backup and gitignore

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@ for d in */; do
   (
     cd $d &&
     echo "Backing up $d"
-    ls -a | grep -vE '(backup|restore).sh' | xargs rm &&   # remove all files except backup.sh and restore.sh
+    ls -A | grep -vE '(backup|restore).sh' | xargs rm &&   # remove all files except backup.sh and restore.sh
     [[ -f ./backup.sh ]] && $(./backup.sh || true) || echo "WARNING: no backup.sh in $d"
   )
 done
